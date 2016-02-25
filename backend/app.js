31196c4546143e5f5267bb9a9327ccba8d09705b
Ryan Douglas <ryan@luckydogdigital.com>
2016-02-25 13:02:07 -0700

    initial commit

---
+++ backend/app.js
@@ -0,0 +1,51 @@
+require('dotenv').load();
+
+var express = require('express');
+var http = require('http');
+var favicon = require('serve-favicon');
+var port = process.env.PORT || 3000;
+
+
+var app = express();
+var server = http.Server(app);
+
+// uncomment after placing your favicon in /public
+//app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
+app.use(express.static('./frontend'));
+
+// catch 404 and forward to error handler
+app.use(function(req, res, next) {
+  var err = new Error('Not Found');
+  err.status = 404;
+  next(err);
+});
+
+// error handlers
+
+// development error handler
+// will print stacktrace
+if (app.get('env') === 'development') {
+  app.use(function(err, req, res, next) {
+    res.status(err.status || 500);
+    res.render('error', {
+      message: err.message,
+      error: err
+    });
+  });
+}
+
+// production error handler
+// no stacktraces leaked to user
+app.use(function(err, req, res, next) {
+  res.status(err.status || 500);
+  res.render('error', {
+    message: err.message,
+    error: {}
+  });
+});
+
+server.listen(port, function(){
+  console.log("Server listening on ", port);
+});
+
+module.exports = app;
