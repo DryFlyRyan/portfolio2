f3af2570daf450085c30aa69cf69073887701fb3
Ryan Douglas <ryan@luckydogdigital.com>
2016-04-10 14:38:45 -0600

    refactoring some html templates.

--- server/app.js
+++ server/app.js
@@ -3,7 +3,7 @@ require('dotenv').load();
 var express = require('express');
 var http = require('http');
 var favicon = require('serve-favicon');
-var port = process.env.PORT || 3001;
+var port = process.env.PORT || 3000;
 
 
 var app = express();
