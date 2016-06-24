096402e82eb49324b6b1dc09d8ae45b93e3ed957
Ryan Douglas <ryan@luckydogdigital.com>
2016-06-24 14:59:47 -0600

    nuked the server. put gulp in. fixed some css issues

--- scripts/controllers/modal.js
+++ scripts/controllers/modal.js
@@ -14,22 +14,14 @@ angular.module('portfolio')
   $scope.age = null;
   $scope.project = project;
 
-  //  This close function doesn't need to use jQuery or bootstrap, because
-  //  the button has the 'data-dismiss' attribute.
   $scope.close = function() {
  	  close({
-    }, 500); // close, but give 500ms for bootstrap to animate
+    }, 500);
   };
 
-  //  This cancel function must use the bootstrap, 'modal' function because
-  //  the doesn't have the 'data-dismiss' attribute.
   $scope.cancel = function() {
-
-    //  Manually hide the modal.
     $element.modal('hide');
-
-    //  Now call close, returning control to the caller.
-    close({}, 500); // close, but give 500ms for bootstrap to animate
+    close({}, 500);
   };
 
 }]);
